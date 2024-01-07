f34b25487589ae94a0bca52f79f7b9f76044c67e
Aboubacar Doucoure <yelounak@gmail.com>
2024-01-07 12:28:56 +0100

    feat: added default value regex

--- src/lib/newlib/classProtocols/update.js
+++ src/lib/newlib/classProtocols/update.js
@@ -14,17 +14,37 @@ export default async (props) => {
 
   try {
     const targetPath = `${folder}/class/protocols.js`
-    let i = cleanProtocols(items)
+    let _items = cleanProtocols(items)
     // let i = items
-    i = i ? i : []
-    let data = `export default ${JSON.stringify(items, null, 4)}`
+    _items = _items ? _items : []
+
+    let content = JSON.stringify(_items, null, 4)
     if (isTemplate) {
-      data = await ejs.render(data, CliNext.payload, {
-        async: true,
-        strict: false
+      content = content.replace(/\<{\{(.*?)\}\}>/g, function (match, token) {
+        let parts = token.split('|')
+        parts = parts.map(i => {
+          if (!i) {
+            return ""
+          }
+          return i.trim()
+        })
+
+        const key = parts[0]
+        const defaultValue = parts[1]
+        if (CliNext.payload[key] !== undefined) {
+          return `<%= ${key} %>`
+        }
+        return `${defaultValue}`
       })
     }
-    CliNext.fs.write({ destination: targetPath, text: data, })
+
+    let data = `export default ${content}`
+    data = await ejs.render(data, CliNext.payload, {
+      async: true,
+      strict: false
+    })
+
+    CliNext.fs.writeText({ destination: targetPath, text: data, })
   } catch (e) {
     console.error(e)
   }
